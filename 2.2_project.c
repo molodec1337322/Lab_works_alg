1da92ada25057ac67bbbf50cc9459261b15687cc
molodec1337322 <54796286+molodec1337322@users.noreply.github.com>
2019-09-11 19:06:06 +0300

    Update 2.2_project.c

--- 2.2_project.c
+++ 2.2_project.c
@@ -6,14 +6,13 @@ int main()
 	float a, b, c;
 	float D, x1, x2;
 	scanf_s("%f %f %f", &a, &b, &c);
-	D = sqrt((b * b) - (4 * a * c));
+	D = (b * b) - (4 * a * c);
 	if (D < 0) {
 		printf("No answers");
 		return 0;
 	}
-	x1 = (-c + D) / (2 * a);
-	x2 = (-c - D) / (2 * a);
-	printf("f%", x1);
-	printf("f%", x2);
+	x1 = (-c + sqrt(D)) / (2 * a);
+	x2 = (-c - sqrt(D)) / (2 * a);
+	printf("\n%f\n%f", x1, x2);
 	return 0;
 }
