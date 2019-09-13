2daa97b3167abe24378fe3dc44df2dc257097b47
molodec1337322 <54796286+molodec1337322@users.noreply.github.com>
2019-09-13 22:15:30 +0300

    Update 3.1_project.c

--- 3.1_project.c
+++ 3.1_project.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+#include <math.h>
 
+int main()
+{
+	int matrix[5][4];
+	int num;
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			scanf_s("%d", &num);
+			matrix[i][j] = num;
+		}
+	}
+
+	int multiply;
+	scanf_s("%d", &multiply);
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			matrix[i][j] *= multiply;
+			printf("%d\t", matrix[i][j]);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
