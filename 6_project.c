cf5afbbe6423e68dac8377d9ec753eaecbcccd03
molodec1337322 <54796286+molodec1337322@users.noreply.github.com>
2019-10-23 12:01:21 +0300

    Update 6_project.c

--- 6_project.c
+++ 6_project.c
@@ -1,1 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+void showArr(int *arr, int size)
+{
+	printf("\n");
+	for (int i = 0; i < size; i++) printf("%d ", arr[i]);
+}
+
+void fillArr(int* arr, int size)
+{
+	srand(0);
+	for (int i = 0; i < size; i++) arr[i] = rand() % 100;
+}
+
+int* gnomeSort(int* arr, int size)
+{
+	int i = 1;
+	int j = 2;
+	int temp;
+	while (i < size)
+	{
+		if (arr[i - 1] > arr[i]) i = j++; // < для сортировки по возрастанию
+		else
+		{
+			temp = arr[i - 1];
+			arr[i - 1] = arr[i];
+			arr[i] = temp;
+			i--;
+			if (i == 0)
+			{
+				i = j++;
+			}
+		}
+		return arr;
+	}
+}
+
+int main()
+{
+	int* arr = malloc(sizeof(int) * 30);
+	fillArr(arr, 30);
+	showArr(arr, 30);
+	arr = gnomeSort(arr, 30);
+	showArr(arr, 30);
+	return 0;
+}
